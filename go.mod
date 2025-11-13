c3dc2d85cf9f33dec3d0aa0158b6375f3aa74321
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-11-13 16:10:37 +0530

    basic shell setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aditya-sutar-45/gosh
+
+go 1.24.9
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		dir, err := os.Getwd()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		currentUser, err := user.Current()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		// for vanity purposes
+		fmt.Printf("%s\n %s > ", dir, currentUser.Username)
+
+		// read input from keyboard
+		inp, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		err = execInp(inp)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+var ErrNoPath = errors.New("path required")
+
+func execInp(inp string) error {
+	// removing newline
+	inp = strings.TrimSuffix(inp, "\n")
+	// getting the args
+	args := strings.Split(inp, " ")
+	command := args[0]
+
+	switch command {
+	case "cd":
+		if len(args) < 2 {
+			return ErrNoPath
+		}
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
+	cmd := exec.Command(command, args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
